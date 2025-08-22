e2974672bd2d835f8b362cde20cb2a31feee3b7a
Ícaro Torres <98storresm@gmail.com>
2025-08-21 22:35:19 -0300

    update: correção de exercícios

--- pythoncodes/python3-cursoemvideo/mundo02/exercícios/ex36.py
+++ pythoncodes/python3-cursoemvideo/mundo02/exercícios/ex36.py
@@ -1,8 +1,8 @@
-casa = float(input('Valor da casa: R$ '))
-salario = float(input('Salário do comprador: R$ '))
+casa = float(input('Valor da casa: R$'))
+salario = float(input('Salário do comprador: R$'))
 anos = int(input('Quantos anos de financiamento? '))
 prestacao = casa / (anos * 12)
 if prestacao >= salario * 0.3:
-    print('O valor do empréstimo seria de R${:.2f}. Empréstimo negado!'.format(prestacao))
+    print('Para pagar {} em {} anos, a prestação empréstimo será de R${:.2f}. Empréstimo negado!'.format(casa, anos, prestacao))
 else:
-    print('O valor do empréstimo é de R${:.2f}. Empréstimo aprovado!'.format(prestacao))
+    print('Para pagar {} em {} anos, a prestação empréstimo será de R${:.2f}. Empréstimo aprovado!'.format(casa, anos, prestacao))
--- pythoncodes/python3-cursoemvideo/mundo02/exercícios/ex39.py
+++ pythoncodes/python3-cursoemvideo/mundo02/exercícios/ex39.py
@@ -1,12 +1,18 @@
 from datetime import date
 
-nascido = int(input('Informe o ano do seu nascimento: '))
 ano = date.today().year
+nascido = int(input('Informe o ano do seu nascimento: '))
 idade = ano - nascido
 
+print('Quem nasceu em {} tem {} anos em {}.'.format(nascido, idade, ano))
+
 if idade < 18:
-    print('Você ainda irá se alistar ao serviço militar. Faltam {} anos.'.format(18 - idade))
+    saldo = 18 - idade
+    print('Você ainda irá se alistar ao serviço militar. Faltam {} anos.'.format(saldo))
+    print('Seu alistamento será em {}.'.format(ano + saldo))
 elif idade == 18:
-    print('Você deve se alistar ao serviço militar este ano.')
+    print('Você deve se alistar ao serviço militar imediatamente.')
 elif idade > 18:
-    print('Você já deveria ter se alistado ao serviço militar há {} anos.'.format(idade - 18))
+    saldo = idade - 18
+    print('Você já deveria ter se alistado ao serviço militar há {} anos.'.format(saldo))
+    print('Seu alistamento foi em {}.'.format(ano - saldo))
