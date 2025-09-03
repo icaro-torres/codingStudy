fcb249846821fe25777579bbf3b5e738eb8b22ce
Ícaro Torres <98storresm@gmail.com>
2025-09-02 21:46:58 -0300

    update: exercícios de python

---
+++ pythoncodes/python3-cursoemvideo/mundo02/exercícios/ex56.py
@@ -0,0 +1,17 @@
+idade_total = 0
+idade_mulheres = 0
+homem_mais_velho = ''
+idade_mais_velho = 0
+for i in range(1, 5):
+    nome = str(input('Digite o nome da {}ª pessoa: '.format(i)))
+    idade = int(input('Digite a idade da {}ª pessoa: '.format(i)))
+    sexo = str(input('Digite o sexo da {}ª pessoa [M/F]: '.format(i))).upper()
+    idade_total += idade
+    if sexo == "M" and idade >= idade_mais_velho:
+        homem_mais_velho = nome
+        idade_mais_velho = idade
+    elif sexo == "F" and idade < 20:
+        idade_mulheres += 1
+media_idades = idade_total / 4
+print('O homem mais velho é {} e sua idade é {}.'.format(homem_mais_velho, idade_mais_velho))
+print('Temos {} mulheres com menos de 20 anos.'.format(idade_mulheres))
